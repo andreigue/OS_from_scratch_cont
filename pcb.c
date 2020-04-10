8e02adbc0d3ccf8fa741800c543078ac0ca7b4ff
Andrei GUEVORKIAN <andrei.guevorkian@mail.mcgill.ca>
2020-04-10 17:47:44 -0400

    replaced with new pcb struct

--- pcb.c
+++ pcb.c
@@ -6,12 +6,27 @@ PC : Points to the the current line reached for that program
 start: Points to first line of that program
 end: Points to last line of that program
 */
+
+
+/*
 typedef struct PCB
 {
     int PC;
     int start;
     int end;
 }PCB;
+*/
+
+
+typedef struct PCB_REC {
+	FILE *PC;
+	int start; // startAddressRAM
+	struct PCB_REC *next;
+	int pageTable[10]; // each cell contains the frame number, 10 because RAM[10]
+	int PC_page; // which page the program is currently at
+	int PC_offset; // which offset (line) of the page we are at
+	int pages_max; // total number of pages
+} PCB;
 
 
 /*
@@ -32,7 +47,22 @@ PCB* makePCB(int start, int end){
 */
 PCB *makePCB(FILE *p, int curPage, int offset, int numberOfPages){
 	PCB *pcb;
+	if (p == NULL) return NULL;
 
+	pcb = (PCB *) malloc(sizeof(PCB));
+	if (pcb == NULL) return NULL;
+
+	pcb->PC    = p;
+	pcb->start = curPage;
+	pcb->next  = NULL;
+	pcb->PC_page = curPage;
+	pcb->PC_offset = offset;
+	pcb->pages_max = numberOfPages;
+
+	int i;
+	for(i = 0; i< 10; i++){
+		pcb->pageTable[i] = -1; 
+	}
 	return pcb;
 }
 
